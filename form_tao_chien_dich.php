2ca22d9ac4b331a5ed82590711b18fed89c27742
Chien Ta <chien.ta@dynagility.com>
2020-09-21 12:00:27 +0700

    first commit

---
+++ form_tao_chien_dich.php
@@ -0,0 +1,15 @@
+<?php /**
+ * Plugin Name:       Form Tao Chien Dich
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Handle form tao chien dich
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Minh Chien
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages
+ */
+
